865de6839e74dc46245674c703c43a2935dcedfc
scurley <35819079+scurley@users.noreply.github.com>
2018-01-25 16:47:39 -0600

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+/**
+ *Author: Eric Wilkening
+ *Date: 01/26/2018
+ *This Program is a simple output program for the GitHub Repository
+ **/
+#include<stdio.h>
+int main(int argc, char **argv){
+  printf("Eric Wilkening\nSamuel Curley");
+return 0;
+}
+//Sam Curley has seen and confirmed this is a working hello world program
